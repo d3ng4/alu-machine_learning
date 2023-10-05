90e927bd80f9ecc234541a5f57a228050625beb1
root <root@Lenovo.localdomain>
2023-10-05 14:09:07 +0300

    commit

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -5,6 +5,9 @@ import numpy as np
 
 
 def convolve_grayscale_same(images, kernel):
+    """
+    Convolves a set of grayscale images with a given kernel using same padding.
+    """
     m, h, w = images.shape
     kh, kw = kernel.shape
 
@@ -13,12 +16,16 @@ def convolve_grayscale_same(images, kernel):
     pad_w = kw // 2
 
     # Pad the images with zeros
-    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
+    padded_images = np.pad(
+        images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant'
+    )
 
     convolved_images = np.zeros_like(images)
 
     for i in range(h):
         for j in range(w):
-            convolved_images[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            convolved_images[:, i, j] = np.sum(
+                padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
+            )
 
     return convolved_images
