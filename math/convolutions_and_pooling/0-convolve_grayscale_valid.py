4184445f4c13f831ec6fb8bca4af228f75cb9411
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-10-05 14:02:00 +0300

    commit

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+functions for convolving grayscale images with a given kernel.
+"""
 
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
-    """
-    Convolves a set of grayscale images with a given kernel using valid padding.
-
-    Args:
-        images (numpy.ndarray): Input grayscale images. Shape: (m, h, w), where m is the number of
-            images, h is the height, and w is the width.
-        kernel (numpy.ndarray): Convolution kernel. Shape: (kh, kw), where kh is the kernel height
-            and kw is the kernel width.
-
-    Returns:
-        numpy.ndarray: Convolved images. Shape: (m, h - kh + 1, w - kw + 1).
-
-    Raises:
-        ValueError: If the dimensions of the images and kernel are incompatible.
-    """
     m, h, w = images.shape
     kh, kw = kernel.shape
 
